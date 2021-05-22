eed8b46410805eb456837a86b3bc24b27961a9d1
Javi <javipin97@gmail.com>
2021-05-21 18:51:01 -0600

    first commit

---
+++ Biblioteca.java
@@ -0,0 +1,14 @@
+
+package biblioteca;
+
+
+public class Biblioteca {
+
+    
+    public static void main(String[] args) {
+        BibliotecaNacional x = new BibliotecaNacional();
+        x.setVisible(true);
+        
+    }
+    
+}
